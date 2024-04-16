87913b005d4ed9b06644a374d5934dba488abfbe
DhruvLimbachiya <dhruvlimbachiya@petpooja.com>
2024-04-16 06:04:04 +0530

    fastlane file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.dhruvv.expensetracker"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "com.dhruvv.expensetracker.HiltTestRunner"
